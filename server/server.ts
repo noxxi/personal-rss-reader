9adde011bd76dea8da2f56e20d2946f29b3485ca
Steffen Ullrich <github@maulwuff.de>
2021-01-06 22:43:27 +0100

    proper check of inputs in server.ts move server.ts to use await instead of .then

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,9 @@
 import express from 'express'
-import bodyParser from 'body-parser';
+import bodyParser, { json } from 'body-parser';
 import RSS from './rss';
-import * as T from './types'
-import debug, * as D from './log'
+import * as T from './types';
+import * as R from 'runtypes';
+import debug, * as D from './log';
 
 let rss = new RSS('data.sqlite');
 let devel_debug = 0;
@@ -31,73 +32,100 @@ let get_items = (req: express.Request, res: express.Response) => {
 app.get('/api/get-items', get_items);
 app.post('/api/get-items', get_items);
 
-app.get('/api/get-feeds', (req, res) => {
-  rss.getXFeeds()
-    .then(feeds => {
-      res.json(feeds)
-    }).catch(why => {
-      res.sendStatus(404);
-      res.json({ error: why });
-    })
+app.get('/api/get-feeds', async (req, res) => {
+  try {
+    let feeds = await rss.getXFeeds();
+    res.json(feeds);
+  } catch(why) {
+    res.sendStatus(404);
+    res.json({ error: why });
+  }
 });
 
-app.post('/api/get-feed', (req, res) => {
-  let rowid = +req.body.rowid;
-  rss.getFeed(rowid)
-    .then(feed => {
-      res.json(feed)
-    }).catch(why => {
-      res.sendStatus(404);
-      res.json({ error: why });
-    })
+app.post('/api/get-feed', async (req, res) => {
+  try {
+    let rowid = +req.body.rowid;
+    if (!rowid) throw "no rowid given";
+    let feed = await rss.getFeed(rowid);
+    res.json(feed);
+  } catch(why) {
+    res.sendStatus(404);
+    res.json({error: why});
+  }
 });
 
-app.post('/api/update-feed', (req, res) => {
-  rss.updFeed(req.body)
-    .then(rowid => {
-      res.json({ rowid: rowid })
-    }).catch(why => {
-      res.sendStatus(404);
-      res.json({ error: why });
-    })
+app.post('/api/update-feed', async (req, res) => {
+  try {
+    let ufeed = R.Record({
+      rowid: R.Number,
+      url: R.String,
+      domain: R.String,
+      title: R.String
+    }).check(req.body);
+    let feed : T.Feed;
+    if (ufeed.rowid) {
+      let rfeed = await rss.getFeed(ufeed.rowid);
+      if (!rfeed) throw `no feed with rowid=${ufeed.rowid}`;
+      feed = { ...rfeed, ...ufeed };
+    } else {
+      feed = { ...ufeed, lastcheck: 0, lastupd: 0, updateInterval: 0 };
+    }
+    let rowid = rss.updFeed(feed);
+    res.json({rowid});
+  } catch(why) {
+    res.sendStatus(404);
+    res.json({error: why});
+  }
 });
 
-app.post('/api/delete-feed', (req, res) => {
-  let rowid = +req.body.rowid;
-  rss.delFeed(rowid)
-    .then(() => {
-      res.json({ result: 'ok'})
-    }).catch(why => {
-      res.sendStatus(404);
-      res.json({ error: why });
-    })
+app.post('/api/delete-feed', async (req, res) => {
+  try {
+    let rowid = +req.body.rowid;
+    if (!rowid) throw "no rowid given";
+    rss.delFeed(rowid);
+    res.json({ result: 'ok'})
+  } catch(why) {
+    res.sendStatus(404);
+    res.json({error: why});
+  }
 });
 
 app.post('/api/set-read', (req, res) => {
-  let items = req.body.items as number[];
-  rss.markItemsRead(items);
-  res.json({ result:'ok'});
+  try {
+    let items = R.Array(R.Number).check(req.body.items);
+    rss.markItemsRead(items);
+    res.json({ result:'ok'});
+  } catch(why) {
+    res.sendStatus(404);
+    res.json({error: why});
+  }
 })
 
 app.post('/api/set-unread', (req, res) => {
-  let items = req.body.items as number[];
-  rss.markItemsUnread(items);
-  res.json({ result:'ok'});
+  try {
+    let items = R.Array(R.Number).check(req.body.items);
+    rss.markItemsUnread(items);
+    res.json({ result:'ok'});
+  } catch(why) {
+    res.sendStatus(404);
+    res.json({error: why});
+  }
 })
 
-app.get('/api/icon/:domain', (req,res) => {
-  console.log(`get icon for ${req.params.domain}`);
-  rss.getIcon(req.params.domain)
-    .then(data => {
-      res.setHeader('Content-Type','image/png');
-      res.setHeader("Cache-Control", "public, max-age=2592000");
-      res.setHeader('Expires', new Date(Date.now() + 2592000000).toUTCString());
-      res.send(data);
-    })
-    .catch(err =>  {
-      res.sendStatus(404);
-      res.json({error: err});
-    });
+app.get('/api/icon/:domain', async (req,res) => {
+  try {
+    let domain = req.params.domain;
+    if (!domain) throw "no domain given";
+    console.log(`get icon for ${domain}`);
+    let data = await rss.getIcon(domain);
+    res.setHeader('Content-Type','image/png');
+    res.setHeader("Cache-Control", "public, max-age=2592000");
+    res.setHeader('Expires', new Date(Date.now() + 2592000000).toUTCString());
+    res.send(data);
+  } catch(why) {
+    res.sendStatus(404);
+    res.json({error: why});
+  }
 });
 
 let updateTimer = setTimeout(updateRSS, 1*1000);
