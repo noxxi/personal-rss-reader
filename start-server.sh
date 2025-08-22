ede352490f996b5c87694b0b010239258d763dd6
Steffen Ullrich <github@maulwuff.de>
2025-08-22 13:42:57 +0200

    remove seclevel=1

--- start-server.sh
+++ start-server.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd server
-NODE_OPTIONS="--insecure-http-parser --tls-cipher-list='DEFAULT:@SECLEVEL=1'"  node server.js
+#NODE_OPTIONS="--insecure-http-parser --tls-cipher-list='DEFAULT:@SECLEVEL=1'"  node server.js
+NODE_OPTIONS="--insecure-http-parser"  node server.js
